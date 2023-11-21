5a1b31bd595b5c035af7d5fa72e87c1e6a877ee8
Curly-stasya <91792338+Curly-stasya@users.noreply.github.com>
2023-11-21 14:41:45 +0300

    Add files via upload

---
+++ Lab2/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Lab2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                NoteBook myNoteBook = new NoteBook();
+                myNoteBook.UserInterface();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+    }
+}
